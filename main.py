b28b0c20ea28d9ed16939e9356c939948db97899
Trần Đức Huy <1712052@student.hcmus.edu.vn>
2020-08-19 00:54:34 +0700

    Up data + Tạo sẵn các file
    - Data: Adult Data Set - UCI Machine Learning Repository
    (https://archive.ics.uci.edu/ml/datasets/Adult)
    
    - Tạo sẵn các file cần thiết: Chỉ cần chạy main.py (chưa tính tới cờ, có gì viết sau)

---
+++ main.py
@@ -0,0 +1,9 @@
+## B1: Phân tích, mô tả dữ liệu (cho báo cáo)
+
+## B2: Làm SVM (do được phép xài thư viện)
+
+## B3: Làm logistic reg (không được xài thư viện, tự code)
+
+## B4: So sánh
+
+## B5: Xuất ra file kết quả (.csv)
